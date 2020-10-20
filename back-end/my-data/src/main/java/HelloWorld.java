5650cb786c633399d848b301329cc6c093bf6ec4
zhiqiu <3164658521@qq.com>
2020-10-20 09:36:41 +0800

    Java基础练习

--- back-end/my-data/src/main/java/HelloWorld.java
+++ back-end/my-data/src/main/java/HelloWorld.java
@@ -1,17 +1,45 @@
 public class HelloWorld {
+
     private boolean flag2;
 
+    public static final String LOG_PUSH = "";
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.aD();
+        testIntLong();
+        testFloatInt();
+        test2();
     }
 
     private void aD() {
         boolean flag = true;
-        int a = 12;
         System.out.println(flag);
+        //boolean默认为false
         System.out.println(flag2);
+    }
+
+    public static void testIntLong() {
+        int a = 14;
+        long b = a;
+        System.out.println(b);
+    }
+
+    //精度丢失
+    public static void testFloatInt() {
+        float a = 14.8f;
+        int b = (int) a;
+        System.out.println(b);
+    }
+
+    public static void test2() {
+        float a = 14.8f;
+        int b = (int) a;
+        System.out.println(b);
     }
 
 }
